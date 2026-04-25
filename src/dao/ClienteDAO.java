c758f31d44e2eeab174fa7e646237f081debecc6
Davidlolomg1 <david.lazaro.marcos0@gmail.com>
2026-04-25 16:04:52 +0200

    feat: Añado funcion de actualizar en clientes

--- src/dao/ClienteDAO.java
+++ src/dao/ClienteDAO.java
@@ -1,65 +1,81 @@
 package dao;
 
-import java.util.List;
-import java.util.ArrayList;
-import models.Cliente;
-import models.Producto;
-
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.sql.PreparedStatement;
+import java.util.ArrayList;
+import java.util.List;
+import models.Cliente;
 
 public class ClienteDAO {
 
-	public List<Cliente> getAll() {
-		List<Cliente> clientes = new ArrayList<>();
-		String sql = "SELECT * FROM clientes";
-		
-		try (Connection conn = Conexion.getConnection()) {
-			Statement stmt = conn.createStatement();
-			ResultSet rs = stmt.executeQuery(sql);
-			
-			while(rs.next()) {
-				Cliente c = new Cliente(
-						rs.getInt("id"), 
-						rs.getString("nombre"), 
-						rs.getString("email"),
-						rs.getString("telefono"),
-						rs.getString("direccion")
-				);
-							
-				clientes.add(c);
-			}
-			
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-		
-		return clientes;
-	}
-	
-	public boolean add(Cliente c) {
-		
-		String sql = "INSERT INTO clientes (nombre, email, telefono, direccion) VALUES (?,?,?,?)";
-		
-		try (Connection conn = Conexion.getConnection()) {
-			PreparedStatement pstmt = conn.prepareStatement(sql);
-			
-			pstmt.setString(1, c.getNombre());
-	        pstmt.setString(2, c.getEmail());
-	        pstmt.setString(3, c.getTelefono());
-	        pstmt.setString(4, c.getDireccion());
-			
-			return pstmt.executeUpdate() > 0;
+    public List<Cliente> getAll() {
+        List<Cliente> clientes = new ArrayList<>();
+        String sql = "SELECT * FROM clientes";
+        
+        try (Connection conn = Conexion.getConnection()) {
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery(sql);
+            
+            while(rs.next()) {
+                Cliente c = new Cliente(
+                        rs.getInt("id"), 
+                        rs.getString("nombre"), 
+                        rs.getString("email"),
+                        rs.getString("telefono"),
+                        rs.getString("direccion")
+                );
+                            
+                clientes.add(c);
+            }
+            
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        
+        return clientes;
+    }
+    
+    public boolean add(Cliente c) {
+        
+        String sql = "INSERT INTO clientes (nombre, email, telefono, direccion) VALUES (?,?,?,?)";
+        
+        try (Connection conn = Conexion.getConnection()) {
+            PreparedStatement pstmt = conn.prepareStatement(sql);
+            
+            pstmt.setString(1, c.getNombre());
+            pstmt.setString(2, c.getEmail());
+            pstmt.setString(3, c.getTelefono());
+            pstmt.setString(4, c.getDireccion());
+            
+            return pstmt.executeUpdate() > 0;
+            
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        
+        return false;
+    }
 
-			
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-		
-		return false;
-	}
-
+    public boolean update(Cliente c) {
+        String sql = "UPDATE clientes SET nombre = ?, email = ?, telefono = ?, direccion = ? WHERE id = ?";
+        
+        try (Connection conn = Conexion.getConnection()) {
+            PreparedStatement pstmt = conn.prepareStatement(sql);
+            
+            pstmt.setString(1, c.getNombre());
+            pstmt.setString(2, c.getEmail());
+            pstmt.setString(3, c.getTelefono());
+            pstmt.setString(4, c.getDireccion());
+            pstmt.setInt(5, c.getId());
+            
+            return pstmt.executeUpdate() > 0;
+            
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
 }
--- src/views/ClientView.java
+++ src/views/ClientView.java
@@ -1,72 +1,87 @@
 package views;
 
+import dao.ClienteDAO;
 import java.util.List;
 import java.util.Scanner;
-import dao.ClienteDAO;
 import models.Cliente;
 
 public class ClientView {
-	private Scanner sc = new Scanner(System.in);
-	private ClienteDAO clienteDAO = new ClienteDAO();
-	
-	public ClientView() {}
-	
-	public void iniciar() {
-		int opcion;
-		do {
-			opcion = this.mostrarMenu();
-			switch(opcion) {
-			
-				case 1 -> this.listarClientes();
-				case 2 -> this.addCliente();
-	
-			}				
-		} while (opcion != 5);
-	}
+    private Scanner sc = new Scanner(System.in);
+    private ClienteDAO clienteDAO = new ClienteDAO();
+    
+    public ClientView() {}
+    
+    public void iniciar() {
+        int opcion;
+        do {
+            opcion = this.mostrarMenu();
+            switch(opcion) {
+                case 1 -> this.listarClientes();
+                case 2 -> this.addCliente();
+                case 3 -> this.actualizarCliente();
+            }               
+        } while (opcion != 5);
+    }
 
-	private int mostrarMenu() {
-		System.out.println("-- Gestión de clientes --");
-		System.out.println("1. Ver todos los clientes");
-		System.out.println("2. Añadir cliente");
-		System.out.println("3. Actualizar cliente");
-		System.out.println("4. Borrar cliente");
-		System.out.println("5. Salir");		
-		int opcion = sc.nextInt();
-		sc.nextLine();
-		
-		return opcion;
-	}
-	
-	private void listarClientes() {
-		List<Cliente> clientes = this.clienteDAO.getAll();
+    private int mostrarMenu() {
+        System.out.println("-- Gestión de clientes --");
+        System.out.println("1. Ver todos los clientes");
+        System.out.println("2. Añadir cliente");
+        System.out.println("3. Actualizar cliente");
+        System.out.println("4. Borrar cliente");
+        System.out.println("5. Salir");    
+        int opcion = sc.nextInt();
+        sc.nextLine();
+        
+        return opcion;
+    }
+    
+    private void listarClientes() {
+        List<Cliente> clientes = this.clienteDAO.getAll();
 
-		for (Cliente c : clientes) {
-			System.out.println(c);
-		}
-		
-	}
-	
-	private void addCliente() {
-	    
-	    System.out.print("Nombre: ");
-	    String nombre = sc.nextLine();
-	    
-	    System.out.print("Email: ");
-	    String email = sc.nextLine();
-	    
-	    System.out.print("Teléfono: ");
-	    String telefono = sc.nextLine();
-	    
-	    System.out.print("Dirección: ");
-	    String direccion = sc.nextLine();
+        for (Cliente c : clientes) {
+            System.out.println(c);
+        }
+    }
+    
+    private void addCliente() {
+        System.out.print("Nombre: ");
+        String nombre = sc.nextLine();
+        System.out.print("Email: ");
+        String email = sc.nextLine();
+        System.out.print("Teléfono: ");
+        String telefono = sc.nextLine();
+        System.out.print("Dirección: ");
+        String direccion = sc.nextLine();
+        Cliente nuevoCliente = new Cliente(nombre, email, telefono, direccion);
+        
+        if (clienteDAO.add(nuevoCliente)) {
+            System.out.println("Cliente insertado correctamente.");
+        } else {
+            System.out.println("No se pudo guardar el cliente.");
+        }
+    }
 
-	    Cliente nuevoCliente = new Cliente(nombre, email, telefono, direccion);
-	    
-	    if (clienteDAO.add(nuevoCliente)) {
-	        System.out.println("Cliente insertado correctamente.");
-	    } else {
-	        System.out.println("No se pudo guardar el cliente.");
-	    }
-	}
-	
+    private void actualizarCliente() {
+        System.out.println("Introduce el ID del cliente a modificar: ");
+        int id = sc.nextInt();
+        sc.nextLine();
+        
+        System.out.println("Nuevo nombre: ");
+        String nombre = sc.nextLine();
+        System.out.println("Nuevo email: ");
+        String email = sc.nextLine();
+        System.out.println("Nuevo teléfono: ");
+        String telefono = sc.nextLine();
+        System.out.println("Nueva dirección: ");
+        String direccion = sc.nextLine();
+        
+        Cliente c = new Cliente(id, nombre, email, telefono, direccion);
+        
+        if(this.clienteDAO.update(c)) {
+            System.out.println("Cliente actualizado correctamente");
+        } else {
+            System.out.println("No se puede actualizar el cliente");
+        }
+    }
 }
